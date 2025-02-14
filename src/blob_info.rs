b5d71e5dc59bf9df5348529aa0bd4b78b3b30d5f
Juan Munoz <juanmunoz890@gmail.com>
2025-02-14 12:17:16 -0300

    add comment specifying `BlobInfo` alias

--- src/blob_info.rs
+++ src/blob_info.rs
@@ -28,14 +28,15 @@ impl From<DisperserG1Commitment> for G1Commitment {
 }
 
 /// Internal of BlobInfo
-/// Contains data related to the blob quorums  
+/// (aliased as EigenDACert in the EigenDA proxy)
+/// Contains data related to the blob quorums
 #[derive(Debug, PartialEq, Clone)]
 pub struct BlobQuorumParam {
     /// The ID of the quorum.
     pub quorum_number: u32,
     /// The max percentage of stake within the quorum that can be held by or delegated to adversarial operators.
     pub adversary_threshold_percentage: u32,
-    /// The min percentage of stake that must attest in order to consider the dispersal successful.    
+    /// The min percentage of stake that must attest in order to consider the dispersal successful.
     pub confirmation_threshold_percentage: u32,
     /// The length of each chunk in bn254 field elements (32 bytes each).
     pub chunk_length: u32,

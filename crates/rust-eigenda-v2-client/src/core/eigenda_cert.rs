a48b41302461f86a5e4c82daa46add0e5326d89d
Juan Munoz <juanmunoz890@gmail.com>
2025-03-20 18:46:15 -0300

    implement compute_blob_key

--- crates/rust-eigenda-v2-client/src/core/eigenda_cert.rs
+++ crates/rust-eigenda-v2-client/src/core/eigenda_cert.rs
@@ -1,9 +1,8 @@
 use std::u16;
 
 use ark_bn254::{G1Affine, G2Affine};
-use ethabi::Token;
+use ethabi::{ParamType, Token};
 use ethereum_types::U256;
-use rust_kzg_bn254_primitives::traits::ReadPointFromBytes;
 use tiny_keccak::{Hasher, Keccak};
 
 use crate::generated::disperser::v2::BlobStatusReply;
@@ -18,6 +17,53 @@ use crate::generated::{
     },
     disperser::v2::BlobInclusionInfo as ProtoBlobInclusionInfo,
 };
+
+#[derive(Debug, PartialEq, Clone)]
+pub(crate) struct G1Commitment {
+    pub(crate) x: Vec<u8>,
+    pub(crate) y: Vec<u8>,
+}
+
+impl From<Vec<u8>> for G1Commitment {
+    // TODO: How many bytes does each field take?
+    // TODO: replace for try_from
+    fn from(value: Vec<u8>) -> Self {
+        let mut x = vec![0u8; 32];
+        let mut y = vec![0u8; 32];
+        x.copy_from_slice(&value[0..32]);
+        y.copy_from_slice(&value[32..64]);
+        G1Commitment { x, y }
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub(crate) struct G2Commitment {
+    pub(crate) x_a0: Vec<u8>,
+    pub(crate) x_a1: Vec<u8>,
+    pub(crate) y_a0: Vec<u8>,
+    pub(crate) y_a1: Vec<u8>,
+}
+
+impl From<Vec<u8>> for G2Commitment {
+    // TODO: How many bytes does each field take?
+    // TODO: replace for try_from
+    fn from(value: Vec<u8>) -> Self {
+        let mut x_a0 = vec![0u8; 32];
+        let mut x_a1 = vec![0u8; 32];
+        let mut y_a0 = vec![0u8; 32];
+        let mut y_a1 = vec![0u8; 32];
+        x_a0.copy_from_slice(&value[0..32]);
+        x_a1.copy_from_slice(&value[32..64]);
+        y_a0.copy_from_slice(&value[64..96]);
+        y_a1.copy_from_slice(&value[96..128]);
+        G2Commitment {
+            x_a0,
+            x_a1,
+            y_a0,
+            y_a1,
+        }
+    }
+}
 
 #[derive(Debug, PartialEq, Clone)]
 pub(crate) struct PaymentHeader {
@@ -58,17 +104,17 @@ impl PaymentHeader {
 
 #[derive(Debug, PartialEq, Clone)]
 pub(crate) struct BlobCommitment {
-    commitment: G1Affine,
-    length_commitment: G2Affine,
-    length_proof: G2Affine,
+    commitment: G1Commitment,
+    length_commitment: G2Commitment,
+    length_proof: G2Commitment,
     length: u32,
 }
 
 impl From<ProtoBlobCommitment> for BlobCommitment {
     fn from(value: ProtoBlobCommitment) -> Self {
-        let commitment = G1Affine::read_point_from_bytes_be(&value.commitment).unwrap();
-        let length_commitment = G2Affine::default(); // G2Affine::deserialize(value.length_commitment); TODO: Implement deserialization
-        let length_proof = G2Affine::default(); // G2Affine::deserialize(value.length_proof.unwrap()); TODO: Implement deserialization
+        let commitment = G1Commitment::from(value.commitment);
+        let length_commitment = G2Commitment::from(value.length_commitment);
+        let length_proof = G2Commitment::from(value.length_proof);
         let length = value.length;
 
         Self {
@@ -227,7 +273,7 @@ impl EigenDACert {
             blob_header.version,
             blob_commitments,
             blob_header.quorum_numbers,
-            blob_header.payment_header_hash
+            blob_header.payment_header_hash,
         );
 
         let blob_key: BlobKey = match blob_key_bytes.try_into() {
@@ -252,10 +298,87 @@ impl EigenDACert {
 pub(crate) type BlobKey = [u8; 32];
 
 fn compute_blob_key_bytes(
-    version: u16,
+    blob_version: u16,
     blob_commitments: BlobCommitment,
     quorum_numbers: Vec<u8>,
-    payment_hash: [u8; 32],
+    payment_metadata_hash: [u8; 32],
 ) -> Vec<u8> {
-    unimplemented!()
+    let mut sorted_quorums = quorum_numbers;
+    sorted_quorums.sort();
+
+    let packed_bytes = ethabi::encode(&[
+        Token::Uint(blob_version.into()), // BlobVersion
+        Token::Bytes(
+            sorted_quorums
+                .iter()
+                .flat_map(|q| q.to_be_bytes())
+                .collect(),
+        ), // SortedQuorums
+        Token::Tuple(vec![
+            // AbiBlobCommitments
+            // Commitment
+            Token::Tuple(vec![
+                Token::Uint(U256::from_big_endian(&blob_commitments.commitment.x)), // commitment X
+                Token::Uint(U256::from_big_endian(&blob_commitments.commitment.y)), // commitment Y
+            ]),
+            // Most cryptography library serializes a G2 point by having
+            // A0 followed by A1 for both X, Y field of G2. However, ethereum
+            // precompile assumes an ordering of A1, A0. We choose
+            // to conform with Ethereum order when serializing a blobHeaderV2
+            // for instance, gnark, https://github.com/Consensys/gnark-crypto/blob/de0d77f2b4d520350bc54c612828b19ce2146eee/ecc/bn254/marshal.go#L1078
+            // Ethereum, https://eips.ethereum.org/EIPS/eip-197#definition-of-the-groups
+            // LengthCommitment
+            Token::Tuple(vec![
+                // X
+                Token::FixedArray(vec![
+                    Token::Uint(U256::from_big_endian(
+                        &blob_commitments.length_commitment.x_a1,
+                    )),
+                    Token::Uint(U256::from_big_endian(
+                        &blob_commitments.length_commitment.x_a0,
+                    )),
+                ]),
+                // Y
+                Token::FixedArray(vec![
+                    Token::Uint(U256::from_big_endian(
+                        &blob_commitments.length_commitment.y_a1,
+                    )),
+                    Token::Uint(U256::from_big_endian(
+                        &blob_commitments.length_commitment.y_a0,
+                    )),
+                ]),
+            ]),
+            // Same as above
+            // LengthProof
+            Token::Tuple(vec![
+                Token::FixedArray(vec![
+                    Token::Uint(U256::from_big_endian(&blob_commitments.length_proof.x_a1)),
+                    Token::Uint(U256::from_big_endian(&blob_commitments.length_proof.x_a0)),
+                ]),
+                Token::FixedArray(vec![
+                    Token::Uint(U256::from_big_endian(&blob_commitments.length_proof.y_a1)),
+                    Token::Uint(U256::from_big_endian(&blob_commitments.length_proof.y_a0)),
+                ]),
+            ]),
+            Token::Uint(blob_commitments.length.into()), // DataLength
+        ]),
+    ]);
+
+    let mut keccak = Keccak::v256();
+    keccak.update(&packed_bytes);
+    let mut header_hash = [0u8; 32];
+    keccak.finalize(&mut header_hash);
+
+    let s2 = vec![
+        Token::FixedBytes(header_hash.to_vec()),
+        Token::FixedBytes(payment_metadata_hash.to_vec()),
+    ];
+
+    let packed_bytes = ethabi::encode(&s2);
+
+    let mut keccak = Keccak::v256();
+    keccak.update(&packed_bytes);
+    let mut blob_key = [0u8; 32];
+    keccak.finalize(&mut blob_key);
+    blob_key.try_into().unwrap() // TODO: handle error
 }

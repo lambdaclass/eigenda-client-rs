10818ae3e0169447e66eb2dcc1f7e7efcfe678de
Juan Munoz <juanmunoz890@gmail.com>
2025-03-21 17:37:13 -0300

    skeleton impl

--- crates/rust-eigenda-v2-client/src/retrieval_client.rs
+++ crates/rust-eigenda-v2-client/src/retrieval_client.rs
@@ -1,1 +1,29 @@
+struct EthClient;
+struct ChainState;
+struct Verifier;
 
+pub(crate) struct RetrievalClient {
+	ethc_client: EthClient,
+	chain_state: ChainState,
+	verifier: Verifier,
+	num_connections: u32,
+}
+
+impl RetrievalClient {
+	pub(crate) fn new(ethc_client: EthClient, chain_state: ChainState, verifier: Verifier, num_connections: u32) -> Self {
+		Self {
+			ethc_client,
+			chain_state,
+			verifier,
+			num_connections,
+		}
+	}
+
+	pub(crate) fn get_blob(&self) {
+    	unimplemented!()
+	}
+
+	pub(crate) fn get_chunks_from_operator(&self) {
+		unimplemented!()
+	}
+}

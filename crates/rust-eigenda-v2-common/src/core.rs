5ecb0b3db2620f3cd25980dfd5edbd2e9cc18387
Juan Munoz <juanmunoz890@gmail.com>
2025-06-13 16:48:09 -0300

    rename file

--- crates/rust-eigenda-v2-common/src/core.rs
+++ crates/rust-eigenda-v2-common/src/core.rs
@@ -1,10 +1,10 @@
 mod blob;
-mod cert_verifier;
+mod check_da_cert_status;
 mod encoded_payload;
 mod payload;
 
 pub use blob::Blob;
-pub use cert_verifier::CheckDACertStatus;
+pub use check_da_cert_status::CheckDACertStatus;
 pub use encoded_payload::EncodedPayload;
 pub use payload::Payload;
 
